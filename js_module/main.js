7b9cf0e897c0971bbcb3153752f382216a94524f
Vlad Bardalez <bardalezvlad@gmail.com>
2015-10-04 13:10:46 -0400

    Added Angular, restructured views

---
+++ js_module/main.js
@@ -0,0 +1,6 @@
+var app = angular.module('Illuminatify');
+
+app.controller('IntrueitiveController', ["$scope", "$routeParams", "$document", "$compile", "$timeout",
+    function ($scope, $routeParams, $document, $compile, $timeout) {
+        console.log(101);
+    }]);
