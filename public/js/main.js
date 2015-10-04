83b9cdb2ce57880e6577293f97f34ff467c6af68
Vlad Bardalez <bardalezvlad@gmail.com>
2015-10-04 13:32:22 -0400

    Lots of changes

---
+++ public/js/main.js
@@ -0,0 +1,6 @@
+var app = angular.module('Illuminatify', []);
+
+app.controller('IlluminatifyController', ["$scope", "$document", "$compile", "$timeout",
+    function ($scope, $document, $compile, $timeout) {
+        console.log(101);
+    }]);
