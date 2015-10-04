60d82463b7949599aac953aeea329bcea699aa08
tslmy <tslimingyang@126.com>
2015-10-04 11:10:30 -0400

    uni-directionally

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
+//Importing packages
 var http = require('http');
+var request = require('request');
 
+function wikidataSearch(name) {
+	
+	result = requests.get('https://www.wikidata.org/w/api.php',params={
+	'action':'wbsearchentities',
+	'search':name,
+	'language':'en',
+	'format':'json'
+	}).json()['search']
+	if result==[]:
+		print 'WARNING: No match found.'
+	return result
+}
+
+
+
+//Main procedure
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
+	res.writeHead(200, {'Content-Type': 'text/plain'});
+	res.end('Hello World\n');
 }).listen(1337, "127.0.0.1");
-
 console.log('Server running at http://127.0.0.1:1337/');
