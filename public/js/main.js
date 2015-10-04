5cdbfed767d0991614b24726bf9413e992b5ce2c
Vlad Bardalez <bardalezvlad@gmail.com>
2015-10-04 15:01:09 -0400

    Updates

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,37 @@
 var app = angular.module('Illuminatify', []);
 
-app.controller('IlluminatifyController', ["$scope", "$document", "$compile", "$timeout",
-    function ($scope, $document, $compile, $timeout) {
+app.controller('IlluminatifyController', ["$scope", "$document", "$compile", "$timeout", "IlluminatifyDataService",
+    function ($scope, $document, $compile, $timeout, IlluminatifyDataService) {
         console.log(101);
+        IlluminatifyDataService.getData("Data")
+            .then(function(result) {
+                console.log(result);
+            }, function() {
+                console.log("Error");
+            })
     }]);
+app.factory("IlluminatifyDataService", ["$http", "$q", function ($http, $q) {
+	var _data = [];			
+
+    var _getData = function (searchTerm) {
+        var deferred = $q.defer();
+        console.log(102);
+        $http.get("/search/" + searchTerm)
+            .then(function (result) {
+                //success
+                console.log(result);
+                angular.copy(result.data, _data);
+                deferred.resolve(_data);
+            },
+            function () {
+                // error
+                deferred.reject();
+            });
+        return deferred.promise;
+    }
+	
+    return {
+		data: _data,
+        getData: _getData
+    };
+}]);
